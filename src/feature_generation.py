c78449a7b05f7e9990275789b6357f34f5f6e31f
Qiana Yang <qyang1997@outlook.com>
2021-05-24 14:19:46 -0500

    continue modifying feature_generation script"

--- src/feature_generation.py
+++ src/feature_generation.py
@@ -11,7 +11,7 @@ def feature_gen(data: pd.DataFrame, train_feature_path: str, train_target_path: 
                 test_feature_path: str, test_target_path: str, seed: int, test_size: int) -> tuple:
     """generate features and labels and write to local csv.
 
-    :param data: :obj: pandas dataframe - training data
+    :param data: :obj: pandas dataframe - data to be fed
     :param train_feature_path: str - file path to save training features
     :param train_target_path: str - file path to save training labels
     :param test_feature_path: str - file path to save test features
@@ -24,18 +24,57 @@ def feature_gen(data: pd.DataFrame, train_feature_path: str, train_target_path: 
         :obj: tuple - tuple of pandas dataframes consisting of features and labels divided into train
                       and test sets
     """
-    features = data.drop('class', axis=1)
+    features = data.drop('class', axis=1)                                      
     target = data['class']
 
-    # add additional features
+    return features, target
+
+
+def add_log_entropy(features: pd.DataFrame) -> pd.DataFrame:
+
     features['log_entropy'] = features.visible_entropy.apply(np.log)
+
+    return features
+
+
+def add_entropy_x_contrast(features: pd.DataFrame) -> pd.DataFrame:
+
     features['entropy_x_contrast'] = features.visible_contrast.multiply(
         features.visible_entropy)
+
+    return features
+
+
+def add_IR_range(features: pd.DataFrame) -> pd.DataFrame:
+
     features['IR_range'] = features.IR_max - features.IR_min
+
+    return features
+
+
+def add_IR_norm_range(features: pd.DataFrame) -> pd.DataFrame:
+
     features['IR_norm_range'] = (features.IR_max - features.IR_min).divide(
         features.IR_mean)
 
-    # split data into test and train
+    return features
+
+
+def train_test_split(features: pd.DataFrame, target: pd.DataFrame, train_feature_path: str,
+                     train_target_path: str, test_feature_path: str, test_target_path: str,
+                     seed: int, test_size: int) -> tuple:
+    """generate features and labels and write to local csv.
+
+    :param features:
+    :param target:
+    :param train_feature_path:
+    :param train_target_path:
+    :param test_feature_path:
+    :param test_target_path:
+    :param seed:
+    :param test_size:
+    :return:
+    """
     X_train, X_test, y_train, y_test = train_test_split(features, target, random_state=seed,
                                                         test_size=test_size)
 
@@ -46,3 +85,5 @@ def feature_gen(data: pd.DataFrame, train_feature_path: str, train_target_path: 
     logger1.info('Features and labels written to file.')
 
     return X_train, X_test, y_train, y_test
+
+
